0e165d376167a38a9a577aefd37432d5d5122fd8
aereal <aereal@aereal.org>
2018-01-23 13:17:42 +0900

    Add tests for initiation

--- app_test.go
+++ app_test.go
@@ -9,6 +9,15 @@ import (
 func TestNewApp(t *testing.T) {
 	var err error
 
+	siteName := "aereal.org"
+	app, err := newApp(strings.Split("magi -name "+siteName+" -url https://aereal.org/", " "), new(bytes.Buffer), new(bytes.Buffer))
+	if err != nil {
+		t.Errorf("app should be successfully initiated but got error: %s", err)
+	}
+	if app.site.name != siteName {
+		t.Errorf("app.site.name should be %s but got %s", siteName, app.site.name)
+	}
+
 	_, err = newApp(strings.Split("magi -unknown", " "), new(bytes.Buffer), new(bytes.Buffer))
 	if err == nil {
 		t.Errorf("app should reject unknown parameter but got %#v", err)
